73a72e175dd83ad9001abe048babcac6b2040205
Sourabh <sourabhkanwade10@gmail.com>
2020-05-18 22:29:28 +0530

    Initial commit

---
+++ app/src/main/java/com/sourabh/location_shower/LocationClass.java
@@ -0,0 +1,31 @@
+package com.sourabh.location_shower;
+
+public class LocationClass {
+    public double latitude;
+    public double longitude;
+
+    public LocationClass() {
+
+    }
+
+    public LocationClass(double latitude, double longitude) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+}
